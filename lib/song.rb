66a3ace1fb4f77029a345961c6a22c13d4930d84
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-16 21:50:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,9 @@ class Song
   attr_accessor :name, :artist, :genre
 
   def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@genres << self.genre
   end
