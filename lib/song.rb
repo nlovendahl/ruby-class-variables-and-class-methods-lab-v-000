4bc2afd6c796c0aa844e318dafbdec895dc22320
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-16 21:58:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,13 @@ class Song
   end
 
   def self.genre_count
-
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
   end
 
   def self.artist_count
